85ea9eebd6737ba924ececd18337fa5404418dd7
Darshan321321 <77149924+Darshan321321@users.noreply.github.com>
2022-01-05 20:07:21 +0530

    solution.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -9,6 +9,14 @@ public class Main {
 
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
     CountsByUsage counts = new CountsByUsage();
+      for(int i=0;i<=cycles.length;i++)
+    {
+      if(cycles[i]<400)
+        lowCount+=1;
+      else if(cycles[i]>=400 && cycles[i]<=919)
+        mediumCount+=1;
+      else if(cycles[i]>=920)
+        highCount+=1;
     return counts;
   }
 
